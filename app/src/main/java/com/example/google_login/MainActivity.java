f3d5827c7e897203e6234b250de52c136e600d9d
Hasan Jamal <hasanjamalabusharekh@gmail.com>
2023-05-20 00:00:37 +0200

    Update

--- app/src/main/java/com/example/google_login/MainActivity.java
+++ app/src/main/java/com/example/google_login/MainActivity.java
@@ -1,30 +1,37 @@
 package com.example.google_login;
 
-import static com.google.android.gms.common.api.GoogleApiClient.*;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.auth.api.Auth;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.Task;
 
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 public class MainActivity extends AppCompatActivity  {
+
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+    private Button loginButton;
 
     GoogleSignInOptions gso;
     GoogleSignInClient gsc;
@@ -34,6 +41,21 @@ public class MainActivity extends AppCompatActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize UI components
+        usernameEditText = findViewById(R.id.username);
+        passwordEditText = findViewById(R.id.password);
+        loginButton = findViewById(R.id.loginbtn);
+
+        // Set click listener for the login button
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = usernameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+                performLogin(username, password);
+            }
+        });
 
         googleBtn = findViewById(R.id.google_btn);
 
@@ -52,16 +74,12 @@ public class MainActivity extends AppCompatActivity  {
                 signIn();
             }
         });
-
-
-
     }
 
     void signIn(){
         Intent signInIntent = gsc.getSignInIntent();
         startActivityForResult(signInIntent,1000);
     }
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode,Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -81,5 +99,59 @@ public class MainActivity extends AppCompatActivity  {
         finish();
         Intent intent = new Intent(MainActivity.this,SecondActivity.class);
         startActivity(intent);
+    }
+
+    private void performLogin(String username, String password) {
+        // Create OkHttpClient instance
+        OkHttpClient client = new OkHttpClient();
+
+        // Build the request body
+        RequestBody requestBody = new FormBody.Builder()
+                .add("Email", username)
+                .add("Password", password)
+                .build();
+
+        // Build the request
+        Request request = new Request.Builder()
+                .url("https://imanshahin-001-site1.gtempurl.com/api/User/login")
+                .post(requestBody)
+                .build();
+
+        // Make the API call
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    // Login successful
+                    String responseBody = response.body().string();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(MainActivity.this, "Login successful!", Toast.LENGTH_SHORT).show();
+                            // Handle successful login, e.g., navigate to the next activity
+                        }
+                    });
+                } else {
+                    // Login failed
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(MainActivity.this, "Login failed. Please try again.", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/google_login/SecondActivity.java
+++ app/src/main/java/com/example/google_login/SecondActivity.java
@@ -1,13 +1,12 @@
 package com.example.google_login;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -48,9 +47,6 @@ public class SecondActivity extends AppCompatActivity {
                 signOut();
             }
         });
-
-
-
     }
 
     void signOut(){
